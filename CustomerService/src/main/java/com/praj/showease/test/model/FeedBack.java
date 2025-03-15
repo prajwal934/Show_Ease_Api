9ed821ef3ef7044edd075bfe604d029cf49a2884
prajwal934 <samalprajwal.302@gmail.com>
2025-03-16 00:52:04 +0530

    update CustomerService

--- CustomerService/src/main/java/com/praj/showease/test/model/FeedBack.java
+++ CustomerService/src/main/java/com/praj/showease/test/model/FeedBack.java
@@ -1,5 +1,7 @@
 package com.praj.showease.test.model;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,12 +13,11 @@ import lombok.NoArgsConstructor;
 @Builder
 public class FeedBack {
 	
-	
-	private String F_id;
-	private String F_movieId;
-	private String F_userId;
-	private String F_rating;
-	private String F_comment;
-	private String F_timeStamp;
+	private String fId;
+	private String movieId;
+	private String custId;
+	private int fRating;
+	private String fComment;
+	private LocalDateTime fTimeStamp;
 
 }
